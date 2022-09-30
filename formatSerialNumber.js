0c5eb9a757eb49e8aa8baeec7108627219c2d5ac
Robi <robi.maliqi@hotmail.co.uk>
2022-09-30 23:41:13 +0100

    finished code and tests

--- formatSerialNumber.js
+++ formatSerialNumber.js
@@ -1,20 +1,29 @@
 const formatSerialNumber = (serial, length) => {
   const removeDash = serial.replace(/-/g, "");
+  if (removeDash.length == 1) {
+    return removeDash.toUpperCase();
+  }
 
-  const evenInput = removeDash
-    .match(new RegExp(".{1," + length + "}", "g"))
-    .join("-");
+  const even = removeDash.length % 2 == 0;
+  const prefix = (!even && removeDash[0] + "-") || "";
+  var output = (even && removeDash) || removeDash.slice(1, removeDash.length);
+  output = output
+    .toUpperCase()
+    .match(new RegExp(".{1," + length + "}", "g")) || [
+    "Please enter a serial number",
+  ];
 
-  const oddOutput = removeDash
-    .slice(1, removeDash.length)
-    .match(new RegExp(".{1," + length + "}", "g"))
-    .join("-");
-
-  if (removeDash.length % 2 == 0) {
-    return evenInput.toUpperCase();
-  } else return `${serial[0]}-${oddOutput}`.toUpperCase();
+  return `${prefix}${output.join("-")}`;
 };
 
-console.log(formatSerialNumber("7Hnwad9Jk", 4)); //Output "7-HNWA-D9Jk"
+module.exports = formatSerialNumber;
+
+console.log(formatSerialNumber("", 1)); //Output "Please enter a serial number"
+console.log(formatSerialNumber("R", 1)); //Output "R"
+console.log(formatSerialNumber("Rw", 1)); //Output "R-W"
+console.log(formatSerialNumber("R-", 1)); //Output "R"
+console.log(formatSerialNumber("R2-", 1)); //Output "R"
+console.log(formatSerialNumber("R25-", 1)); //Output "R"
+console.log(formatSerialNumber("7Hnwad9Jk", 4)); //Output "7-HNWA-D9J"
 console.log(formatSerialNumber("6F2k-1d-0-z", 4)); //Output "6F2K-1D0Z"
 console.log(formatSerialNumber("14k-9-b", 2)); //Output "1-4K-9B"
---
+++ formatSerialNumber.test.js
@@ -0,0 +1,39 @@
+const formatSerialNumber = require("./formatSerialNumber");
+
+describe("formatSerialNumber", () => {
+  it("throws error when there is an empty string entered", () => {
+    expect(formatSerialNumber("", 1)).toEqual("Please enter a serial number");
+  });
+
+  it("returns a output with a single character and 1 length", () => {
+    expect(formatSerialNumber("R", 1)).toEqual("R");
+  });
+
+  it("returns a output with a two characters(one lowercase) and 1 length", () => {
+    expect(formatSerialNumber("Rw", 1)).toEqual("R-W");
+  });
+
+  it("returns a output with one character, a dash and 1 length", () => {
+    expect(formatSerialNumber("R-", 1)).toEqual("R");
+  });
+
+  it("returns a output with two characters, a dash and 1 length", () => {
+    expect(formatSerialNumber("R2-", 1)).toEqual("R-2");
+  });
+
+  it("returns a output with three characters, a dash and 1 length", () => {
+    expect(formatSerialNumber("R25-", 1)).toEqual("R-2-5");
+  });
+
+  it("returns a output with eight characters, no dash, uppercase and lowercase and 4 length", () => {
+    expect(formatSerialNumber("7Hnwad9Jk", 4)).toEqual("7-HNWA-D9JK");
+  });
+
+  it("returns a output with eight characters, a few dashes, uppercase and lowercase and 4 length", () => {
+    expect(formatSerialNumber("6F2k-1d-0-z", 4)).toEqual("6F2K-1D0Z");
+  });
+
+  it("returns a output with eight characters, a few dashes, uppercase and lowercase and 4 length", () => {
+    expect(formatSerialNumber("14k-9-b", 2)).toEqual("1-4K-9B");
+  });
+});
